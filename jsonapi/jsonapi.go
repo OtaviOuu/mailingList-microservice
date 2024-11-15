34cee15065d73f131ef96a71e5effc4e875d6c5e
OtaviOuu <conta.otaviou@gmail.com>
2024-11-14 23:28:27 -0300

    feat: function to read data from request body

--- jsonapi/jsonapi.go
+++ jsonapi/jsonapi.go
@@ -1,9 +1,20 @@
 package jsonapi
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"net/http"
 )
 
 func setJsonHeader(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+}
+
+// io.Reader -> qualquer coisa que pode ser lida
+func fromJson[T any](body io.Reader, target T) error {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(body)
+
+	json.Unmarshal(buf.Bytes(), &target)
 }
